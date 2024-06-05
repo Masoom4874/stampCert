6c03258e47768f6455edb4531f308a6a0b73138c
Masoom4874 <masoom4874@gmail.com>
2024-06-06 01:08:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import { useState, useRef } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import baseImg from "./assets/baseImage.jpg";
+import jsPDF from "jspdf";
+import { Form, Row, Col, Button } from "react-bootstrap";
+
+const CertificationPortal = () => {
+  const [name, setName] = useState("");
+  const [stamp, setStamp] = useState(null);
+  const [pdfPreviewUrl, setPdfPreviewUrl] = useState("");
+  const iframeRef = useRef();
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleStampChange = (e) => {
+    setStamp(e.target.files[0]);
+  };
+
+  // Creating Certificate
+  const handleCreateCertificate = () => {
+    if (!name) {
+      alert("Please Enter Name");
+      return;
+    }
+    if (!stamp) {
+      alert("Please Upload Stamp");
+      return;
+    }
+
+    const doc = new jsPDF("landscape");
+    doc.addImage(baseImg, "JPEG", 0, 0, 297, 210);
+
+    doc.setFontSize(24);
+    doc.text(name, 148.5, 120, { align: "center" });
+
+    if (stamp) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const imgData = event.target.result;
+        doc.addImage(imgData, "PNG", 168, 130, 50, 50, { align: "center" });
+        const pdfUrl = doc.output("bloburl");
+        setPdfPreviewUrl(pdfUrl);
+      };
+      reader.readAsDataURL(stamp);
+    } else {
+      const pdfUrl = doc.output("bloburl");
+      setPdfPreviewUrl(pdfUrl);
+    }
+  };
+
+  return (
+    <div className="main-page">
+      <h2>Certification Portal</h2>
+
+      <Row className="align-items-end mb-5">
+        <Col>
+          <h4>Please Enter Name</h4>
+          <Form.Control
+            placeholder="Enter Name"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </Col>
+
+        <Col>
+          <h4>Please Select Stamp</h4>
+          <Form.Control
+            type="file"
+            accept="image/*"
+            onChange={handleStampChange}
+          />
+        </Col>
+        <Col>
+          <Button onClick={handleCreateCertificate}>Create Certificate</Button>
+        </Col>
+      </Row>
+
+      <h3>Certification Preview</h3>
+      {pdfPreviewUrl && (
+        <Row className="mt-5">
+          <Col>
+            <iframe
+              ref={iframeRef}
+              src={pdfPreviewUrl}
+              width="100%"
+              height="500px"
+              title="PDF Preview"
+            />
+          </Col>
+        </Row>
+      )}
+    </div>
+  );
+};
+
+export default CertificationPortal;
